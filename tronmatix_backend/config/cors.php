7aac6a3c8644793f5d85a3a2d37315dfeb9b8341
COPYProgrammer168 <ahjm29253@gmail.com>
2026-03-18 14:53:18 +0700

    Update CORS configuration to use environment variable

--- tronmatix_backend/config/cors.php
+++ tronmatix_backend/config/cors.php
@@ -11,7 +11,7 @@ return [
         'http://localhost:5174',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
-        'https://tronmatix-webtest.onrender.com/'
+         env('FRONTEND_URL', 'https://tronmatix-frontend.onrender.com'),
     ],
 
     'allowed_origins_patterns' => [],
