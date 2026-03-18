7cc080b1e5b64782ddd2aba2d255f7384d25007b
COPYProgrammer168 <srannboss@gmail.com>
2026-03-18 11:32:09 +0700

    TRX_V1.0U

--- tronmatix_backend/config/cors.php
+++ tronmatix_backend/config/cors.php
@@ -8,7 +8,6 @@ return [
         'http://localhost:5174',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
-        'https://*.netlify.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
