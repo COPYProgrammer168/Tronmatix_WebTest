2587b53a12aff494f127938dc5c234d50ad4c05f
COPYProgrammer168 <ahjm29253@gmail.com>
2026-03-19 22:09:48 +0700

    Update cors.php

--- tronmatix_backend/config/cors.php
+++ tronmatix_backend/config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
